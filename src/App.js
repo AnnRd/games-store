01b49877feb79db3b6e538d84a15655de129e8bc
Anna Radchenko <anrd.rs>
2022-07-18 13:49:24 +0300

    styles updated and react router dom installed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      app
+      {/* <header className="App-header">
         app
-      </header>
+      </header> */}
     </div>
   );
 }
