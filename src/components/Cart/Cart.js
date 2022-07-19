f7d9f3ea8966750e4c66a11ced34511ba938db13
Anna Radchenko <anrd.rs>
2022-07-19 15:48:03 +0300

    cart menu updated with usestate

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,19 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import { BiCartAlt } from 'react-icons/bi';
 import CartMenu from '../CartMenu/CartMenu';
 import './Cart.scss';
 
 
 function Cart () {
+    const [isCartMenuVisible, setCartMenuVisible] = useState(false);
     const items = useSelector(state => state.cart.itemsInCart); //массив игр, которые пользователь выбрал для добавления в корзину
     const totalPrice = items.reduce((acc, game) => acc += game.price, 0);
     return (
         <div className="cart">
-            <BiCartAlt size={25} className="cart-icon"/>
-            <span className="cart-price">{totalPrice} ₽</span>
-            <CartMenu items={items} onClick={() => null} totalPrice={totalPrice}/>
+            <BiCartAlt size={25} className="cart-icon" onClick={() => setCartMenuVisible(!isCartMenuVisible)}/>
+            {totalPrice > 0 ? <span className="cart-price">{totalPrice} ₽</span> : null}
+            {isCartMenuVisible ? <CartMenu items={items} onClick={() => null} totalPrice={totalPrice}/> : null}
         </div>
     )
 }
